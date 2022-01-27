53aa1884875d630090081495d622ce0712b76477
João Vitor <joaovitormrn@gmail.com>
2022-01-27 11:50:10 -0300

    Implementado comportamento de edição para os produtos salvos via API

--- app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
+++ app/src/main/java/br/com/alura/estoque/repository/ProdutoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.estoque.repository;
 
 import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -53,27 +55,46 @@ public class ProdutoRepository {
         .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
-    public void salva(Produto produto, DadosCarregadosListener<Produto> listener) {
+    public void salva(Produto produto, DadosCarregadosCallback<Produto> callback) {
+        salvaNaApi(produto, callback);
+    }
+
+    private void salvaNaApi(Produto produto, DadosCarregadosCallback<Produto> callback) {
         Call<Produto> call = service.salva(produto);
         call.enqueue(new Callback<Produto>() {
             @Override
-            public void onResponse(Call<Produto> call, Response<Produto> response) {
-                Produto produtoSaolvo = response.body();
-                new BaseAsyncTask<>(() -> {
-                    long id = dao.salva(produtoSaolvo);
-                    return dao.buscaProduto(id);
-                }, listener::quandoCarregados)
-                        .execute();
+            public void onResponse(@NonNull Call<Produto> call, @NonNull Response<Produto> response) {
+                if(response.isSuccessful()){
+                    Produto produtoSaolvo = response.body();
+                    if(produtoSaolvo != null){
+                        salvaInterno(produtoSaolvo, callback);
+                    }
+                }else{
+                    callback.quandoFalha("Resposta não sucedida");
+                }
             }
 
             @Override
             public void onFailure(Call<Produto> call, Throwable t) {
-
+                callback.quandoFalha("Falha de comunicação: " + t.getMessage());
             }
         });
     }
 
+    private void salvaInterno(Produto produto, DadosCarregadosCallback<Produto> callback) {
+        new BaseAsyncTask<>(() -> {
+            long id = dao.salva(produto);
+            return dao.buscaProduto(id);
+        }, callback::quandoSucesso)
+                .execute();
+    }
+
     public interface DadosCarregadosListener <T> {
         void quandoCarregados(T resultado);
+    }
+
+    public interface DadosCarregadosCallback <T>{
+        void quandoSucesso(T resultado);
+        void quandoFalha(String erro);
     }
 }
